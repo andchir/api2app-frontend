a4b85543588d8f1f844257c68b4a69455134f8de
andrew <andchir@gmail.com>
2024-01-03 01:42:46 +0300

    links text-ellipsis

--- src/app/shared/pipes/to-html.pipe.ts
+++ src/app/shared/pipes/to-html.pipe.ts
@@ -11,10 +11,10 @@ export class ToHtmlPipe implements PipeTransform {
         const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/ig;
         const emailRegex = /[^\s]+@[^\s.]+\.[^\s]+/ig;
         text = text.replace(emailRegex, (value) => {
-            return `<a class="text-blue-500 underline hover:text-blue-700" href="mailto:${value}">${value}</a>`;
+            return `<a class="inline-block max-w-full whitespace-nowrap overflow-hidden text-ellipsis text-blue-500 underline hover:text-blue-700" href="mailto:${value}">${value}</a>`;
         });
         text = text.replace(urlRegex, (value) => {
-            return `<a class="text-blue-500 underline hover:text-blue-700" rel="nofollow" href="${value}" target="_blank">${value}</a>`;
+            return `<a class="inline-block max-w-full whitespace-nowrap overflow-hidden text-ellipsis text-blue-500 underline hover:text-blue-700" rel="nofollow" href="${value}" target="_blank">${value}</a>`;
         });
         return text;
     }
