526aee6bb4585910b4295e5b5d3e78f3bbeb092c
andrew <andchir@gmail.com>
2024-10-06 00:19:19 +0300

    store user token in localStorage

--- src/app/services/token-storage.service.ts
+++ src/app/services/token-storage.service.ts
@@ -13,14 +13,14 @@ export class TokenStorageService {
     constructor() { }
 
     signOut(): void {
-        window.sessionStorage.removeItem(TOKEN_KEY);
-        window.sessionStorage.removeItem(REFRESHTOKEN_KEY);
-        window.sessionStorage.removeItem(USER_KEY);
+        window.localStorage.removeItem(TOKEN_KEY);
+        window.localStorage.removeItem(REFRESHTOKEN_KEY);
+        window.localStorage.removeItem(USER_KEY);
     }
 
     saveToken(token: string): void {
-        window.sessionStorage.removeItem(TOKEN_KEY);
-        window.sessionStorage.setItem(TOKEN_KEY, token);
+        window.localStorage.removeItem(TOKEN_KEY);
+        window.localStorage.setItem(TOKEN_KEY, token);
 
         const user = this.getUser();
         if (user?.username) {
@@ -29,25 +29,25 @@ export class TokenStorageService {
     }
 
     getToken(): string | null {
-        return window.sessionStorage.getItem(TOKEN_KEY);
+        return window.localStorage.getItem(TOKEN_KEY);
     }
 
     saveRefreshToken(token: string): void {
-        window.sessionStorage.removeItem(REFRESHTOKEN_KEY);
-        window.sessionStorage.setItem(REFRESHTOKEN_KEY, token);
+        window.localStorage.removeItem(REFRESHTOKEN_KEY);
+        window.localStorage.setItem(REFRESHTOKEN_KEY, token);
     }
 
     getRefreshToken(): string | null {
-        return window.sessionStorage.getItem(REFRESHTOKEN_KEY);
+        return window.localStorage.getItem(REFRESHTOKEN_KEY);
     }
 
     saveUser(user: any): void {
-        window.sessionStorage.removeItem(USER_KEY);
-        window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+        window.localStorage.removeItem(USER_KEY);
+        window.localStorage.setItem(USER_KEY, JSON.stringify(user));
     }
 
     getUser(): User {
-        const user = window.sessionStorage.getItem(USER_KEY);
+        const user = window.localStorage.getItem(USER_KEY);
         if (user) {
             return JSON.parse(user);
         }
