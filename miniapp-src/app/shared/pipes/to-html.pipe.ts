55d323f7462ecebb26054f660f1665e41bf739fa
andrew <andchir@gmail.com>
2025-08-20 01:55:14 +0300

    upd

--- miniapp-src/app/shared/pipes/to-html.pipe.ts
+++ miniapp-src/app/shared/pipes/to-html.pipe.ts
@@ -14,11 +14,11 @@ export class ToHtmlPipe implements PipeTransform {
         const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/ig;
         const emailRegex = /[^\s]+@[^\s.]+\.[^\s]+/ig;
         text = text.replace(emailRegex, (value) => {
-            return `<a class="inline-block max-w-full whitespace-nowrap overflow-hidden text-ellipsis text-blue-500 underline hover:text-blue-700" href="mailto:${value}">${value}</a>`;
+            return `<a class="inline-block align-bottom max-w-full whitespace-nowrap overflow-hidden text-ellipsis text-blue-500 underline hover:text-blue-700" href="mailto:${value}">${value}</a>`;
         });
         text = text.replace(urlRegex, (value) => {
             const target = value.includes('#') ? '_self' : '_blank';
-            return `<a class="inline-block max-w-full whitespace-nowrap overflow-hidden text-ellipsis text-blue-500 underline align-top hover:text-blue-700" rel="nofollow" href="${value}" target="${target}">${value}</a>`;
+            return `<a class="inline-block align-bottom max-w-full whitespace-nowrap overflow-hidden text-ellipsis text-blue-500 underline align-top hover:text-blue-700" rel="nofollow" href="${value}" target="${target}">${value}</a>`;
         });
 
         return text;
