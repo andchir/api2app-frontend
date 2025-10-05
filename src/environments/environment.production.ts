7d0af5b6cb9849a19d0657251fe435110801b4aa
andrew <andchir@gmail.com>
2025-10-06 00:03:37 +0300

    upd

--- src/environments/environment.production.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: true,
-    appName: 'Api2App',
-    languages: ['en', 'ru', 'fr', 'de', 'es'],
-    apiUrl: '',
-    robokassaUrl: ''
-};
-
-export const BASE_URL = environment.apiUrl === '/' ? `${window.location.origin}/` : environment.apiUrl;
