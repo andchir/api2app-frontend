495e0ecdec17f9183f668f143eb16133fb950203
andchir <andchir@gmail.com>
2026-04-18 14:30:31 +0300

    element options - add queryParameterName field

--- miniapp-src/app/apps/models/app-block.interface.ts
+++ miniapp-src/app/apps/models/app-block.interface.ts
@@ -16,6 +16,7 @@ export interface AppBlockElementOptions {
     outputApiUuid?: string;
     outputApiFieldName?: string|number;
     outputApiFieldType?: string;
+    queryParameterName?: string;
 }
 
 export interface AppBlockElement {
