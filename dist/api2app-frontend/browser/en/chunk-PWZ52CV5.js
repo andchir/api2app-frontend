9fde41fcf50e19e0345b73514a0dad206d816f34
andrew <andchir@gmail.com>
2026-07-27 01:50:04 +0300

    pause active player on app hide

--- dist/api2app-frontend/browser/en/chunk-PWZ52CV5.js
+++
@@ -1,2 +0,0 @@
-import{b as a}from"./chunk-5YATJDY6.js";import"./chunk-I3UUFYCO.js";import"./chunk-HVVGLSBD.js";import"./chunk-XM6MNJFN.js";export{a as AppsModule};
-/**i18n:bc71f4e667be5a7faee6efbbc51b401fb399ee1b30cda80a1d56778e4c0a38ef*/
