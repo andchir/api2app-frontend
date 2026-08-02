b5ac71fe7294956332c0c288f1df07458218ff83
andrew <andchir@gmail.com>
2026-08-02 23:33:32 +0300

    build

---
+++ dist/api2app-frontend/browser/ru/chunk-MJLPBK2V.js
@@ -0,0 +1,2 @@
+import{b as a}from"./chunk-RPOCLTIE.js";import"./chunk-R2TADS3N.js";import"./chunk-RCFYZSW4.js";import"./chunk-XM6MNJFN.js";export{a as AppsModule};
+/**i18n:bc71f4e667be5a7faee6efbbc51b401fb399ee1b30cda80a1d56778e4c0a38ef*/
