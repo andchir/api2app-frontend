7896d6b1a9dcae21e94a76a1c03496b89a340fbe
andrew <andchir@gmail.com>
2026-08-02 23:14:03 +0300

    table - parse json fix

--- src/app/apps/components/elements/table-element/table-element.component.ts
+++ src/app/apps/components/elements/table-element/table-element.component.ts
@@ -76,8 +76,8 @@ export class TableElementComponent implements ControlValueAccessor {
                 if (value && typeof value === 'object' && !Array.isArray(value)) {
                     row[key] = '';
                     Object.keys(value).forEach(function(k) {
-                        row[key] += k + ":\n";
-                        row[key] += value[k] + ":\n";
+                        row[key] += k + ": ";
+                        row[key] += value[k] + "\n";
                     });
                 }
             }
