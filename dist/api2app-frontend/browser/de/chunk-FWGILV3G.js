4a78f3a61de922d0d4c06b25269d7ac6f99a5fb2
andchir <andchir@gmail.com>
2026-08-03 19:00:24 +0300

    upd

--- dist/api2app-frontend/browser/de/chunk-FWGILV3G.js
+++
@@ -1,2 +0,0 @@
-import{b as a}from"./chunk-7FJE7LYP.js";import"./chunk-KQ2FGT4H.js";import"./chunk-7NWLEMTC.js";import"./chunk-XM6MNJFN.js";export{a as AppsModule};
-/**i18n:bc71f4e667be5a7faee6efbbc51b401fb399ee1b30cda80a1d56778e4c0a38ef*/
