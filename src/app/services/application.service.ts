fb33839f6c8ca68eba5c74d62bc527805cf273dc
andrew <andchir@gmail.com>
2026-08-04 01:26:23 +0300

    onElementValueChanged - upd

--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -208,7 +208,7 @@ export class ApplicationService extends DataService<ApplicationItem> {
             case 'table':
                 return element.valueArr;
         }
-        return ApplicationService.createStringValue(element, element.value);
+        return element.value ? String(element.value) : null;
     }
 
     private static parseLocalStorageData(value: string | null): Record<string, any> | null {
