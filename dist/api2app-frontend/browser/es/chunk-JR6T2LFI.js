7f3e50c16652c4d9c6ec36eaa7ee781dc61d93e9
andrew <andchir@gmail.com>
2026-08-10 15:28:09 +0300

    user profile - vk pay save data

---
+++ dist/api2app-frontend/browser/es/chunk-JR6T2LFI.js
@@ -0,0 +1,2 @@
+import{b as a}from"./chunk-JDTG6TFW.js";import"./chunk-VFQCV5SS.js";import"./chunk-6FLMYG6K.js";import"./chunk-2B3GOZLP.js";export{a as AppsModule};
+/**i18n:e2639f991f5fe4b222d34901e7de8068766b29039b6689250fcd536c3bba5a68*/
--- dist/api2app-frontend/browser/ru/chunk-Y4AFNKDW.js
+++
@@ -1,2 +0,0 @@
-import{b as a}from"./chunk-LGHPPST2.js";import"./chunk-G74IXB3S.js";import"./chunk-2X4NNGAG.js";import"./chunk-CQ6RHPKQ.js";export{a as AppsModule};
-/**i18n:50518fd3fd1383eb2cf0ace9f6d8962bdf7750f98380ba35b77c5ce3bb8c9743*/
