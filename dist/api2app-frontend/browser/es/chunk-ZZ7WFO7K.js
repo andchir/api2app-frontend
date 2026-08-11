117eed661f5eda0056a62a1b13ae55c506e6aa84
andrew <andchir@gmail.com>
2026-08-11 23:52:27 +0300

    websocket url fix

--- dist/api2app-frontend/browser/es/chunk-ZZ7WFO7K.js
+++
@@ -1,2 +0,0 @@
-import{b as a}from"./chunk-XI544YDG.js";import"./chunk-V35IS62E.js";import"./chunk-JLHEAPJ3.js";import"./chunk-C74GAOGG.js";export{a as AppsModule};
-/**i18n:9937a8ce741bed5ecb012666bde2aac09e18154f2a19178fffa89076f60c7d37*/
