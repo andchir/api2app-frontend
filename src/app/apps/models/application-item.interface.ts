5f51545547f5789c6ebbce6b71544d5c2fec1b4b
andrew <andchir@gmail.com>
2026-08-12 02:02:12 +0300

    app - service key

--- src/app/apps/models/application-item.interface.ts
+++ src/app/apps/models/application-item.interface.ts
@@ -21,6 +21,7 @@ export interface ApplicationItem {
     maintenance?: boolean;
     vkAppId?: string;
     vkSecretKey?: string;
+    vkServiceKey?: string;
     vkToken?: string;
     tgBotToken?: string;
     tgForwardToUserId?: number;
