2662a0394d3fc27ae1f25aa6e919f2a1e3e1fc85
andrew <andchir@gmail.com>
2026-08-12 22:51:54 +0300

    upd

--- dist/api2app-frontend/browser/es/chunk-FRZAW4JR.js
+++
@@ -1,2 +0,0 @@
-import{c as a}from"./chunk-AS6UAAB5.js";import"./chunk-5NZ7JKVD.js";import"./chunk-NJ6SRJVT.js";import"./chunk-ZHQ3D4TN.js";export{a as AppsModule};
-/**i18n:215068213d01a9d6456595561b6abc7586b6c249e6287ab7be75ad9bc4afb707*/
