c5a6876b7cfbcfc27e0420fbbd958085781eb267
andrew <andchir@gmail.com>
2026-08-12 03:14:24 +0300

    AppActionComponent - code refactoring - use signals

--- dist/api2app-frontend/browser/ru/chunk-IE5JF3V2.js
+++
@@ -1,2 +0,0 @@
-import{b as a}from"./chunk-FOGI6KP2.js";import"./chunk-SCTLKO4G.js";import"./chunk-UYOMUTGA.js";import"./chunk-ZHQ3D4TN.js";export{a as AppsModule};
-/**i18n:215068213d01a9d6456595561b6abc7586b6c249e6287ab7be75ad9bc4afb707*/
