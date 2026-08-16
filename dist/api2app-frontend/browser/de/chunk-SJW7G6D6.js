697da1495417c2454ea1f644968c7c7c68858d27
andrew <andchir@gmail.com>
2026-08-16 20:52:55 +0300

    field validation upd

--- dist/api2app-frontend/browser/de/chunk-SJW7G6D6.js
+++
@@ -1,2 +0,0 @@
-import{c as a}from"./chunk-SJHYASGB.js";import"./chunk-FYLE5R3D.js";import"./chunk-HBUZYTFO.js";import"./chunk-3DYT7SWD.js";export{a as AppsModule};
-/**i18n:f6fc5eaff9b64ece3c2ad270a9864f6a2a55585f5238e653372402057e804e9f*/
---
+++ dist/api2app-frontend/browser/en/chunk-Q5JMJPOI.js
@@ -0,0 +1,2 @@
+import{c as a}from"./chunk-F5I7YFOM.js";import"./chunk-JM2YGV2U.js";import"./chunk-HBUZYTFO.js";import"./chunk-3DYT7SWD.js";export{a as AppsModule};
+/**i18n:f6fc5eaff9b64ece3c2ad270a9864f6a2a55585f5238e653372402057e804e9f*/
