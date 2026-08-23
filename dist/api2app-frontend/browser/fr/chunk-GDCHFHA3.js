37d9bd7ce3136dedc0d33d24f449e58df1d34a0d
andrew <andchir@gmail.com>
2026-08-23 23:50:33 +0300

    local store value fix

---
+++ dist/api2app-frontend/browser/fr/chunk-GDCHFHA3.js
@@ -0,0 +1,2 @@
+import{c as a}from"./chunk-6Y7APS72.js";import"./chunk-G7KUQUUP.js";import"./chunk-MMISZN3N.js";import"./chunk-3DYT7SWD.js";export{a as AppsModule};
+/**i18n:f6fc5eaff9b64ece3c2ad270a9864f6a2a55585f5238e653372402057e804e9f*/
--- dist/api2app-frontend/browser/ru/chunk-OAF4EJ3W.js
+++
@@ -1,2 +0,0 @@
-import{c as a}from"./chunk-65SYF7F6.js";import"./chunk-B4TAX2O6.js";import"./chunk-MMISZN3N.js";import"./chunk-3DYT7SWD.js";export{a as AppsModule};
-/**i18n:f6fc5eaff9b64ece3c2ad270a9864f6a2a55585f5238e653372402057e804e9f*/
