082636807ef1de493bbe724fafba3a43aa7c2751
andchir <andchir@gmail.com>
2026-08-26 17:54:45 +0300

    input-tags - allowAdd option

---
+++ dist/api2app-frontend/browser/uk/chunk-BUPTVDYA.js
@@ -0,0 +1,2 @@
+import{c as a}from"./chunk-RWHUO3DY.js";import"./chunk-SCQWBX46.js";import"./chunk-AUULX3OH.js";import"./chunk-3Y3UR2P7.js";export{a as AppsModule};
+/**i18n:d7eb9f9dd1331eac06d76f1c49863457677439175ef742e72302141d2e91b157*/
