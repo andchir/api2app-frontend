6fa19fe93b9a6c71d9294593cf0fd4d85a78271a
andchir <andchir@gmail.com>
2026-08-27 12:45:41 +0300

    app-input-tags-element - refactoring

---
+++ dist/api2app-frontend/browser/uk/chunk-3A3LEJEL.js
@@ -0,0 +1,2 @@
+import{c as a}from"./chunk-ZTAF5CJJ.js";import"./chunk-KXFQKTQD.js";import"./chunk-N4VCAUA2.js";import"./chunk-ZIIJNMNP.js";export{a as AppsModule};
+/**i18n:c0d03b9242d56e2b8f160447d4dac594faff11883540d30b4b9396b418b8368a*/
