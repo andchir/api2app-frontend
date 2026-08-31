64fd3653f21c9f78c61ece046f449b1544221e2a
andrew <andchir@gmail.com>
2026-09-01 02:21:26 +0300

    form switch element value

---
+++ dist/api2app-frontend/browser/uk/chunk-WAZP5AEV.js
@@ -0,0 +1,2 @@
+import{c as a}from"./chunk-PGRS4F2A.js";import"./chunk-FBTCUSQZ.js";import"./chunk-IB7F42L5.js";import"./chunk-ZIIJNMNP.js";export{a as AppsModule};
+/**i18n:c0d03b9242d56e2b8f160447d4dac594faff11883540d30b4b9396b418b8368a*/
