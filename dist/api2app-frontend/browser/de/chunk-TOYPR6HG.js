83f67ea4104ef74073ce92f93b9251d5b4b32652
andchir <andchir@gmail.com>
2026-09-05 19:55:51 +0300

    build

---
+++ dist/api2app-frontend/browser/de/chunk-TOYPR6HG.js
@@ -0,0 +1,2 @@
+import{c as a}from"./chunk-TC26RVBE.js";import"./chunk-IXJ2DJNY.js";import"./chunk-VZDUAEYY.js";import"./chunk-S4D5N4ZE.js";export{a as AppsModule};
+/**i18n:f279e508e9ef7387710693894b2fe19c800d321e878c51e71485e2c14cb708ca*/
