b92250a150b9cc5506275874229967ecec7caebb
andchir <andchir@gmail.com>
2026-09-14 15:20:29 +0300

    clear values upd

--- dist/api2app-frontend/browser/es/chunk-TURWS3IS.js
+++
@@ -1,2 +0,0 @@
-import{c as a}from"./chunk-3IJH6LXP.js";import"./chunk-A2N5R26S.js";import"./chunk-VZDUAEYY.js";import"./chunk-S4D5N4ZE.js";export{a as AppsModule};
-/**i18n:f279e508e9ef7387710693894b2fe19c800d321e878c51e71485e2c14cb708ca*/
