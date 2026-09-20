01657fd5e5054ba89defd7c0ab88697a8fd90be2
andchir <andchir@gmail.com>
2026-09-20 16:32:04 +0300

    AppAdultValidationComponent update

--- dist/api2app-frontend/browser/ru/chunk-YQDUL3ZA.js
+++
@@ -1,2 +0,0 @@
-import{c as a}from"./chunk-XA7HUNDU.js";import"./chunk-HT5JHMPA.js";import"./chunk-5JTI3X5A.js";import"./chunk-S4D5N4ZE.js";export{a as AppsModule};
-/**i18n:f279e508e9ef7387710693894b2fe19c800d321e878c51e71485e2c14cb708ca*/
