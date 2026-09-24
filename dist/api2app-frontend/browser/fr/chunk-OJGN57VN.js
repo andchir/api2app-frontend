5729a43db3d7dca1b577ccc1b444badc66beb5ac
andchir <andchir@gmail.com>
2026-09-24 14:04:16 +0300

    save app slug value

---
+++ dist/api2app-frontend/browser/fr/chunk-OJGN57VN.js
@@ -0,0 +1,2 @@
+import{c as a}from"./chunk-S6YOLWWL.js";import"./chunk-JBNWYOHL.js";import"./chunk-HYVFP57G.js";import"./chunk-3SRXBNTU.js";export{a as AppsModule};
+/**i18n:b533aefbbe899529b83757883f2ff056f7d902f95b23204a99d724e51ff6ee97*/
